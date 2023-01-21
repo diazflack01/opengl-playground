8e881f735d631a1f3752cdb52f16411b1580d1d4
Kelvin M. Robles <kelvin.robles018@gmail.com>
2023-01-21 18:36:56 +0800

    setup imgui library

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 #include "Shader.hpp"
 #include "Camera.hpp"
 #include "Utils.hpp"
