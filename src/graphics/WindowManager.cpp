306c4c46a09344d97307e10ad5010c31edc80ebf
Kelvin M. Robles <kelvin.robles018@gmail.com>
2023-04-30 09:29:10 +0800

    basic WindowManager abstraction

---
+++ src/graphics/WindowManager.cpp
@@ -0,0 +1,94 @@
+#include <WindowManager.hpp>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+WindowManager::WindowManager(int width, int height, std::string title)
+ : m_width{width}
+ , m_height{height}
+ , m_title{std::move(title)} {
+
+    if (glfwInit() == GLFW_FALSE) {
+        throw std::runtime_error("Failed to initialize GLFW");
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    m_window = glfwCreateWindow(width, height, m_title.c_str(), nullptr, nullptr);
+
+    if (m_window == nullptr) {
+        glfwTerminate();
+        throw std::runtime_error("Failed to create GLFW window");
+    }
+
+    glfwMakeContextCurrent(m_window);
+
+    glfwSetFramebufferSizeCallback(m_window, framebufferSizeCallback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        throw std::runtime_error("Failed to initialize GLAD");
+    }
+
+    glViewport(0, 0, m_width, m_height);
+}
+
+WindowManager::~WindowManager() {
+    glfwTerminate();
+}
+
+void WindowManager::update() {
+    glfwPollEvents();
+
+    processKeyboardInputs(m_window);
+
+    m_isCloseRequested = glfwWindowShouldClose(m_window);
+}
+
+bool WindowManager::isCloseRequested() const {
+    return m_isCloseRequested;
+}
+
+void WindowManager::setWidth(int width) {
+    m_width = width;
+    updateWindowSize();
+}
+
+void WindowManager::setHeight(int height) {
+    m_height = height;
+    updateWindowSize();
+}
+
+void WindowManager::setTitle(const std::string &title) {
+    m_title = title;
+    glfwSetWindowTitle(m_window, m_title.c_str());
+}
+
+int WindowManager::getWidth() const {
+    return m_width;
+}
+
+int WindowManager::getHeight() const {
+    return m_height;
+}
+
+const std::string &WindowManager::getTitle() const {
+    return m_title;
+}
+
+void WindowManager::processKeyboardInputs(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void WindowManager::swapBuffers() {
+    glfwSwapBuffers(m_window);
+}
+
+void WindowManager::updateWindowSize() {
+    glfwSetWindowSize(m_window, m_width, m_height);
+    glViewport(0, 0, m_width, m_height);
+}
---
+++ src/graphics/include/graphics/WindowManager.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+#include <glad/glad.h> // needs to be included before glfw
+#include <GLFW/glfw3.h>
+
+class WindowManager {
+public:
+    WindowManager(int width, int height, std::string title);
+
+    WindowManager(const WindowManager&) = delete;
+    WindowManager& operator=(const WindowManager&) = delete;
+    WindowManager(WindowManager&&) = delete;
+    WindowManager& operator=(WindowManager&&) = delete;
+    ~WindowManager();
+
+    void update();
+
+    [[nodiscard]] bool isCloseRequested() const;
+
+    void swapBuffers();
+
+    void setWidth(int width);
+    void setHeight(int height);
+    void setTitle(const std::string& title);
+
+    [[nodiscard]] int getWidth() const;
+    [[nodiscard]] int getHeight() const;
+    [[nodiscard]] const std::string &getTitle() const;
+
+private:
+    static void processKeyboardInputs(GLFWwindow* window);
+    void updateWindowSize();
+
+    int m_width;
+    int m_height;
+    std::string m_title;
+    GLFWwindow* m_window{nullptr};
+    bool m_isCloseRequested{false};
+};
