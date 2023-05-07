c63abe18d2358480ead6b0018340634e1d536b01
Kelvin M. Robles <kelvin.robles018@gmail.com>
2023-05-07 09:57:27 +0800

    raytrace one wkend - simplify hit sphere equation

--- src/demo-apps/raytracing-in-one-weekend/main.cpp
+++ src/demo-apps/raytracing-in-one-weekend/main.cpp
@@ -8,15 +8,17 @@
 double hit_sphere(const point3& center, double radius, const ray& r) {
     // Analytic solution - https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection.html
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
+    // simplify equation by substituting `b = 2h`
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
+
     if (discriminant < 0) {
         return -1.0;
     } else {
         // quadratic equation to solve for actual `t` distance to get intersection point in sphere
-        return (-b - sqrt(discriminant) ) / (2.0*a);
+        return (-half_b - sqrt(discriminant) ) / (2.0*a);
     }
 }
 
