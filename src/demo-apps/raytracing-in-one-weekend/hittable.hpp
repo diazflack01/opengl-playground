0128f85f964e610b6d0913af4b0b16f3618a3381
Kelvin M. Robles <kelvin.robles018@gmail.com>
2023-05-07 10:31:32 +0800

    raytrace one wkend - front face info in hit_record

--- src/demo-apps/raytracing-in-one-weekend/hittable.hpp
+++ src/demo-apps/raytracing-in-one-weekend/hittable.hpp
@@ -6,6 +6,16 @@ struct hit_record {
     point3 p; // intersection point
     vec3 normal;
     double t; // distance of intersection point from camera ray
+    bool front_face;
+
+    /*!
+     * Always make member attribute normal pointing against the ray. This approach
+     * we can determine the side of surface at the time of coloring.
+     */
+    void set_face_normal(const ray& r, const vec3& outward_normal) {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal :-outward_normal;
+    }
 };
 
 class hittable {
