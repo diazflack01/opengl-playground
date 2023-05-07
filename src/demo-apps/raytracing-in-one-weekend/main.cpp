7971ae6ec36f8db3e5b3f0f6b369daed00ae3e92
Kelvin M. Robles <kelvin.robles018@gmail.com>
2023-05-07 16:53:40 +0800

    raytrace one wkend - render spheres with lambertian and metal materials

--- src/demo-apps/raytracing-in-one-weekend/main.cpp
+++ src/demo-apps/raytracing-in-one-weekend/main.cpp
@@ -8,14 +8,9 @@
 #include "utils.hpp"
 #include "hittable_list.hpp"
 #include "camera.hpp"
+#include "material.hpp"
 
-enum class DiffuseRayReflectScatterType {
-    UNIT_SPHERE,
-    UNIT_VECTOR_SPHERE,
-    HEMISPHERE,
-};
-
-color ray_color(const ray& r, const hittable& world, int depth = 4, DiffuseRayReflectScatterType diffuseType = DiffuseRayReflectScatterType::HEMISPHERE) {
+color ray_color(const ray& r, const hittable& world, int depth = 4) {
     hit_record rec;
 
     if (depth <= 0)
@@ -23,23 +18,13 @@ color ray_color(const ray& r, const hittable& world, int depth = 4, DiffuseRayRe
 
     // Fix shadow acne by using 0.001
     if (world.hit(r, 0.001, infinity, rec)) {
-        const auto diffuseReflectRay = [&]{
-            switch (diffuseType) {
-                // UNIT_SPHERE, UNIT_VECTOR_SPHERE is offset of intersection point from normal
-                case DiffuseRayReflectScatterType::UNIT_SPHERE:
-                    return random_in_unit_sphere();
-                case DiffuseRayReflectScatterType::UNIT_VECTOR_SPHERE:
-                    return random_unit_vector();
-                // does not rely on intersection point
-                case DiffuseRayReflectScatterType::HEMISPHERE:
-                    return random_in_hemisphere(rec.normal);
-            }
-        }();
-
-        point3 target = rec.p + rec.normal + diffuseReflectRay;
-        // `target - rec.p`, random ray bounce from intersection point
-        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
+        ray scattered;
+        color attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+            return attenuation * ray_color(scattered, world, depth-1);
+        return color(0,0,0);
     }
+
     vec3 unit_direction = unit_vector(r.direction()); // unit vector will be in range `-1.0 < val < 1.0`
     auto t = 0.5*(unit_direction.y() + 1.0); // map (-1,1) to (0, 1)
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0); // LERP/linear interpolation/blend `(1-t) * startValue + t * endValue`
@@ -55,8 +40,16 @@ int main() {
 
     // World
     hittable_list world;
-    world.add(std::make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(std::make_shared<sphere>(point3(0,-100.5,-1), 100));
+
+    auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = std::make_shared<lambertian>(color(0.7, 0.3, 0.3));
+    auto material_left   = std::make_shared<metal>(color(0.8, 0.8, 0.8));
+    auto material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2));
+
+    world.add(std::make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(std::make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    world.add(std::make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(std::make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Camera
     camera cam;
