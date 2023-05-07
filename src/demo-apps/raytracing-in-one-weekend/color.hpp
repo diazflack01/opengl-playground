e4b4c1f39d2d5503bca8dcac5cf502815dcf7280
Kelvin M. Robles <kelvin.robles018@gmail.com>
2023-05-07 16:05:22 +0800

    raytrace one wkend - gamma correct and fix shadow acne for diffuse render

--- src/demo-apps/raytracing-in-one-weekend/color.hpp
+++ src/demo-apps/raytracing-in-one-weekend/color.hpp
@@ -6,15 +6,12 @@
 #include <algorithm>
 
 void write_color(std::ostream &out, color pixel_color, int samples_per_pixel = 1.0) {
-    auto r = pixel_color.x();
-    auto g = pixel_color.y();
-    auto b = pixel_color.z();
-
     // Divide the color by the number of samples.
     auto scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    // Apply gamma correction by raising to the power of `1/gamma`. Below will use gamma `2`.
+    auto r = std::sqrt(scale * pixel_color.x());
+    auto g = std::sqrt(scale * pixel_color.y());
+    auto b = std::sqrt(scale * pixel_color.z());
 
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(256 * std::clamp(r, 0.0, 0.999)) << ' '
--- src/demo-apps/raytracing-in-one-weekend/main.cpp
+++ src/demo-apps/raytracing-in-one-weekend/main.cpp
@@ -15,7 +15,8 @@ color ray_color(const ray& r, const hittable& world, int depth = 4) {
     if (depth <= 0)
         return color{0,0,0};
 
-    if (world.hit(r, 0, infinity, rec)) {
+    // Fix shadow acne by using 0.001
+    if (world.hit(r, 0.001, infinity, rec)) {
         // (1) rec.p + rec.normal: vector from intersection point to unit sphere tangent to it
         // (2) from (1), get random point within unit sphere distance
         point3 target = rec.p + rec.normal + vec3::random_in_unit_sphere();
