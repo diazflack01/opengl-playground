d43745efa26e3767cabdb3fab35a54861d02776a
Kelvin M. Robles <kelvin.robles018@gmail.com>
2023-05-07 11:07:51 +0800

    raytrace one wkend - render image with 100 samples per pixel

--- src/demo-apps/raytracing-in-one-weekend/main.cpp
+++ src/demo-apps/raytracing-in-one-weekend/main.cpp
@@ -24,6 +24,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -31,14 +32,7 @@ int main() {
     world.add(std::make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    camera cam;
 
     // Render
     std::ofstream ofs;
@@ -48,12 +42,16 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cout << "Scanlines remaining: " << j << '\n';
         for (int i = 0; i < image_width; ++i) {
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
-            auto viewport_pixel = lower_left_corner + u*horizontal + v*vertical;
-            ray r(origin,  viewport_pixel - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(ofs, pixel_color);
+            // A pixel is 1x1 in size of whatever unit, random sampling within this size
+            // to get color will reduce jaggedness. Very basic antialiasing approach.
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(ofs, pixel_color, samples_per_pixel);
         }
     }
     std::cout << "\nDone.\n";
