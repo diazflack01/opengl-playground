b1451ea145e0ce4d128b1054c45978df9fce4514
Kelvin M. Robles <kelvin.robles018@gmail.com>
2023-08-19 13:17:51 +0800

    add point lights in demo model app

--- src/demo-apps/model-loading/main.cpp
+++ src/demo-apps/model-loading/main.cpp
@@ -19,6 +19,7 @@ float lastFrame = 0.0f; // Time of last frame
 const float SCREEN_WIDTH = 800.0f * 1.5;
 const float SCREEN_HEIGTH = 600.0f * 1.5;
 const auto WINDOW_TITLE = "demo-model-loading";
+constexpr glm::mat4 IDENTITY_MATRIX{1.0f};
 
 float mousePosX = 0.0;
 float mousePosY = 0.0;
@@ -72,9 +73,78 @@ int main(int argc, char** argv) {
     glfwSetCursorPosCallback(window, mouseCallback);
     glfwSetScrollCallback(window, scrollCallback);
 
+    float cubeVertices[] = {
+            // positions          // normals           // texture coords
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+            0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+            0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+    };
+
+    // create VAO, VBO, EBO
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    // bind VAO first, so all VBO, EBO buffer calls will be recorded
+    glBindVertexArray(VAO);
+    // bind buffer & set data - VBO, EBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+
+    // set VAO data layout attributes for vertex shader usage
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // unbind VAO
+    glBindVertexArray(0);
+
+    Shader lightCubeShader{"resources/shader/demo_phong_lighting_light.vert", "resources/shader/demo_phong_lighting_light.frag"};
     Shader modelWithLightingShader{"resources/shader/model_loading.vert", "resources/shader/model_loading_with_lighting.frag"};
     Model vampireModel{"resources/models/vampire/dancing_vampire.dae"};
-    Model backPackModel{"resources/models/backpack/backpack.obj"};
+    // Model backPackModel{"resources/models/backpack/backpack.obj"};
 
     glEnable(GL_DEPTH_TEST);
 
@@ -94,14 +164,40 @@ int main(int argc, char** argv) {
         const glm::mat4 view = camera.getViewMatrix();
         const glm::mat4 projection = glm::perspective(glm::radians(camera.getFieldOfView()), SCREEN_WIDTH/SCREEN_HEIGTH, 0.1f, 100.0f);
 
+        // draw light cube
+        glBindVertexArray(VAO);
+        const auto currTime = glfwGetTime();
+        // const glm::vec3 lightPos{sin(currTime) * 1.2f, cos(currTime) * 1.0f, sin(currTime) * 2.0f};
+        const auto lightCubeScaleMatrix = glm::scale(IDENTITY_MATRIX, glm::vec3{0.2f});
+        const std::vector<glm::vec3> lightPositions = {{-5.f + (sin(currTime) * 3.f), 5.f, -10.f}, {-5.f, 2.f + (cos(currTime) * 3.0f), -11.f}, {-5.f + (sin(currTime) * 3.f), -4.f, -10.f}};
+        lightCubeShader.use();
+        lightCubeShader.setMat4("view", view);
+        lightCubeShader.setMat4("projection", projection);
+        for (const auto& lightPos : lightPositions) {
+            const auto lightCubeTranslateMatrix = glm::translate(IDENTITY_MATRIX, lightPos);
+            lightCubeShader.setMat4("model", lightCubeTranslateMatrix * lightCubeScaleMatrix);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        glBindVertexArray(0);
+
         modelWithLightingShader.use();
         // set shader lighting uniforms
         modelWithLightingShader.setFloat("material.shininess", 32.0f);
-        modelWithLightingShader.setInt("numPointLights", 0);
+        modelWithLightingShader.setInt("numPointLights", lightPositions.size());
+        for (auto i = 0u; i < lightPositions.size(); i++) {
+            const auto pointLightPrefixStr = "pointLights[" + std::to_string(i) + "]";
+            modelWithLightingShader.setVec3Float(pointLightPrefixStr + ".position", lightPositions[i].x, lightPositions[i].y, lightPositions[i].z);
+            modelWithLightingShader.setFloat(pointLightPrefixStr + ".constant", 1.0f);
+            modelWithLightingShader.setFloat(pointLightPrefixStr + ".linear", 0.09f);
+            modelWithLightingShader.setFloat(pointLightPrefixStr + ".quadratic", .032f);
+            modelWithLightingShader.setVec3Float(pointLightPrefixStr + ".ambient", 0.2f, 0.2f, 0.2f);
+            modelWithLightingShader.setVec3Float(pointLightPrefixStr + ".diffuse", 0.5f, 0.5f, 0.5f);
+            modelWithLightingShader.setVec3Float(pointLightPrefixStr + ".specular", 1.0f, 1.0f, 1.0f);
+        }
         modelWithLightingShader.setBool("hasSpotLight", false);
         modelWithLightingShader.setVec3Float("directionalLight.direction", -0.2f, -1.0f, -0.3f);
-        modelWithLightingShader.setVec3Float("directionalLight.ambient", 0.2f, 0.2f, 0.2f);
-        modelWithLightingShader.setVec3Float("directionalLight.diffuse", 0.5f, 0.5f, 0.5f);
+        modelWithLightingShader.setVec3Float("directionalLight.ambient", 0.01f, 0.01f, 0.01f);
+        modelWithLightingShader.setVec3Float("directionalLight.diffuse", 0.1f, 0.1f, 0.1f);
         modelWithLightingShader.setVec3Float("directionalLight.specular", 1.0f, 1.0f, 1.0f);
         modelWithLightingShader.setVec3Float("viewPos", camera.getPosition().x, camera.getPosition().y, camera.getPosition().z);
         // set shared view, projection value
@@ -109,22 +205,20 @@ int main(int argc, char** argv) {
         modelWithLightingShader.setMat4("view", view);
         // draw vampire
         const auto vampireModelMatrix = []{
-            const auto identityMatrix = glm::mat4{1.0f};
-            const auto scaleMatrix = glm::scale(identityMatrix, glm::vec3{0.05f, 0.05f, 0.05f});
-            const auto translateMatrix = glm::translate(identityMatrix, glm::vec3{-5.0f, -5.0f, -13.0f});
+            const auto scaleMatrix = glm::scale(IDENTITY_MATRIX, glm::vec3{0.05f, 0.05f, 0.05f});
+            const auto translateMatrix = glm::translate(IDENTITY_MATRIX, glm::vec3{-5.0f, -5.0f, -13.0f});
             return translateMatrix * scaleMatrix;
         }();
         modelWithLightingShader.setMat4("model", vampireModelMatrix);
         vampireModel.draw(modelWithLightingShader);
         // draw backpack
         const auto backModelMatrix = []{
-            const auto identityMatrix = glm::mat4{1.0f};
-            const auto rotationMatrix = glm::rotate(identityMatrix, glm::radians(180.0f), glm::vec3{.0f, 1.0f, .0f});
-            const auto translateMatrix = glm::translate(identityMatrix, glm::vec3{3.0f, .0f, -3.0f});
+            const auto rotationMatrix = glm::rotate(IDENTITY_MATRIX, glm::radians(180.0f), glm::vec3{.0f, 1.0f, .0f});
+            const auto translateMatrix = glm::translate(IDENTITY_MATRIX, glm::vec3{3.0f, .0f, -3.0f});
             return translateMatrix;
         }();
         modelWithLightingShader.setMat4("model", backModelMatrix);
-        backPackModel.draw(modelWithLightingShader);
+        // backPackModel.draw(modelWithLightingShader);
 
         glfwSwapBuffers(window);
     }
